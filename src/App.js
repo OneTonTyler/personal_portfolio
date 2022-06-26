8b22db46496d4584dddbc7465514dcd6c6573e34
Tyler <OneTonTyler@pm.me>
2022-06-25 21:57:11 -0600

    Initiated a basic skeleton for my webpage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Blog, Home, Projects, Resume } from './containers';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/resume" element={<Resume />} />
+                </Routes>
+            </Router>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/containers/index.js
@@ -0,0 +1,25 @@
+/* Blog page
+ * This page is dedicated towards all the lessons I have learned during my projects.
+ * This page tracks my current progress of my projects, so that I can maintain frequent updates so that viewers can see
+ * how my current projects are progressing.
+ */
+export { default as Blog } from './blog/blog';
+
+/* Home page
+ * This page is dedicated to my about me section.
+ * Since this is the first page viewers will see, it should be the most polished, but short.
+ */
+export { default as Home } from './home/home';
+
+/* Projects page
+ * This page houses a writeup of my projects. What design decisions I took, and why.
+ * What the project showcases;
+ * What I can do;
+ * What I learned.
+ */
+export { default as Projects } from './projects/projects';
+
+/* Resume page
+ * This page displays my current skills and work experience
+ */
+export { default as Resume } from './resume/resume';
