df0a40e5fdacc5fa9ec7837a3c19fe96f3976a2e
Tyler <OneTonTyler@pm.me>
2022-06-26 21:56:36 -0600

    moved

--- server/common/services/sql.services.js
+++ server/common/services/sql.services.js
@@ -1,21 +1,30 @@
-require('dotenv').config({path: './server/common/config/.env'});
-
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-        port: '3306',
-        host: 'localhost',
-        user: 'OneTonTyler',
-        password: 'SecretPassword',
-        database: 'projects'
-});
-
-exports.connect = () => {
-    connection.connect(function (err) {
-        if (err) {
-            console.error('error connecting: ' + err.stack);
-        }
-
-        console.log('connected as id ' + connection.threadId);
-    });
-};
+// require('dotenv').config({path: './server/common/config/.env'});
+//
+// const mysql = require('mysql');
+//
+// const connection = mysql.createConnection({
+//         port: '3306',
+//         host: 'localhost',
+//         user: 'OneTonTyler',
+//         password: 'SecretPassword',
+//         database: 'projects'
+// });
+//
+// exports.connect = function() {
+//     connection.connect(function (err) {
+//         if (err) {
+//             console.error('error connecting: ' + err.stack);
+//             process.exit(1);
+//         }
+//         console.log('Successfully connected to the database.');
+//     });
+// };
+//
+// exports.query = () => {
+//     connection.query('SELECT * FROM projects.project_table', (err, rows) => {
+//         if (err) throw err;
+//
+//         console.log('Data received from database:');
+//         console.log(rows);
+//     });
+// };
