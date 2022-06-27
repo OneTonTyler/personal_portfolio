d392314112b03b5d8a5e4fc84bdcceeb0ce3097b
Tyler <OneTonTyler@pm.me>
2022-06-26 21:56:07 -0600

    Testing creating new entries with sql

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
-const database = require('./common/services/sql.services');
+const database = require('./database/model/user.model');
 const express = require('express');
 
 const app = express();
 app.use(express.json());
 
 database.connect();
+database.newEntry([[
+    2,
+    'My Second Entry',
+    'This is my second entry',
+    'A test to verify my sql queries',
+    '20220626',
+    '20220626',
+    'PROJECT'
+]]);
 
 app.listen(8000, () => {
     console.log('Listening');
