a7d9c1e6ba97f3cc866de3f282a3f70ead1f80c6
Tyler <OneTonTyler@pm.me>
2022-06-27 16:25:47 -0600

    Setting aside for eventually adding user access.

---
+++ server/database/model/user.model.js
@@ -0,0 +1,10 @@
+/* This is where I will implement a user model... eventually.
+ *
+ * User Schema
+ * ---- User_1          | Tables are connected with a foreign-key
+ *      User_1_Table_1  | This way a user will only have access to their individual tables and those related to it
+ *      User_1_Table_2  |
+ * ---- User_2
+ *      User_2_Table_1
+ *      User_2_Table_2
+ */
