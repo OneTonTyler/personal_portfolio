dcdcb8f18efeb6e59e3c3a19e3e3285116362f24
Tyler <OneTonTyler@pm.me>
2022-06-26 21:56:24 -0600

    refactored

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,7 +1,7 @@
 /* Blog page
- * This page is dedicated towards all the lessons I have learned during my projects.
- * This page tracks my current progress of my projects, so that I can maintain frequent updates so that viewers can see
- * how my current projects are progressing.
+ * This page is dedicated towards all the lessons I have learned during my database.
+ * This page tracks my current progress of my database, so that I can maintain frequent updates so that viewers can see
+ * how my current database are progressing.
  */
 export { default as Blog } from './blog/blog';
 
@@ -12,7 +12,7 @@ export { default as Blog } from './blog/blog';
 export { default as Home } from './home/home';
 
 /* Projects page
- * This page houses a writeup of my projects. What design decisions I took, and why.
+ * This page houses a writeup of my database. What design decisions I took, and why.
  * What the project showcases;
  * What I can do;
  * What I learned.
