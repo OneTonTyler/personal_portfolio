2e8ba2a32d66341737054f12c26c7090f9812672
Tyler <OneTonTyler@pm.me>
2022-06-27 22:21:48 -0600

    Integrated routes.config.js

--- server/index.js
+++ server/index.js
@@ -4,19 +4,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-database.connect();
-database.newEntry([[
-    0,
-    'My Second Entry',
-    'This is my second entry',
-    'A test to verify my sql queries',
-    '20220626',
-    '20220626',
-]], 'projects');
+const DatabaseRoutes = require('./database/routes.config');
 
-database.getByTable('projects');
-database.patchById('projects', ['TITLE', 'DESCRIPTION', 'ID'], ['This is a Patch', 'Patching an entry', 10], 0);
-database.removeById('projects', 0);
+DatabaseRoutes.routes_config(app);
 
 app.listen(8000, () => {
     console.log('Listening');
