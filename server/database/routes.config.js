40ace6c7cc57d6f9297b6d5c3a6f201a104ee940
Tyler <OneTonTyler@pm.me>
2022-06-27 22:22:00 -0600

    Integrated routes.config.js

--- server/database/routes.config.js
+++ server/database/routes.config.js
@@ -1,1 +1,20 @@
+// Controllers
+const ContentController = require('./controllers/content.controller');
 
+exports.routes_config = (app) => {
+    app.get('/foo', [
+        ContentController.list
+    ]);
+
+    app.post('/foo', [
+        ContentController.insert
+    ]);
+
+    app.patch('/foo', [
+        ContentController.patchById
+    ]);
+
+    app.delete('/foo', [
+        ContentController.removeById
+    ]);
+};
