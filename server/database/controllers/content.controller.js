0c0783742920b56e930599410530eb879e3c69c3
Tyler <OneTonTyler@pm.me>
2022-06-27 22:20:15 -0600

    Created controller to manage CRUD requests

---
+++ server/database/controllers/content.controller.js
@@ -0,0 +1,62 @@
+// Models
+const ContentModel = require('../model/content.model');
+
+// Connect to database
+ContentModel.connect();
+
+// Insert a new project or blog post
+exports.insert = async (req, res) => {
+    const table_name = req.body.table;
+    const values = req.body.values;
+
+    await ContentModel.newEntry(table_name, [values])
+        .then((results) => {
+            res.status(200).send({message: results});
+        })
+        .catch((err) => {
+            res.status(400).send({errorMessage: err});
+        });
+};
+
+// List all entries
+exports.list = async (req, res) => {
+    const table_name = req.body.table;
+
+    await ContentModel.getByTable(table_name)
+        .then((results) => {
+            res.status(200).send({message: results});
+        })
+        .catch((err) => {
+            res.status(400).send({errorMessage: err});
+        });
+};
+
+// Update selected entry
+exports.patchById = async (req, res) => {
+    const table_name = req.body.table;
+    const cols = req.body.cols;
+    const values = req.body.values;
+    const id = req.body.id;
+
+    await ContentModel.patchById(table_name, cols, values, id)
+        .then((results) => {
+            res.status(200).send({message: results});
+        })
+        .catch((err) => {
+            res.status(400).send({errorMessage: err});
+        });
+};
+
+// Delete selected entry
+exports.removeById = async (req, res) => {
+    const table_name = req.body.table;
+    const id = req.body.id;
+
+    await ContentModel.removeById(table_name, id)
+        .then((results) => {
+            res.status(200).send({message: results});
+        })
+        .catch((err) => {
+            res.status(400).send({errorMessage: err});
+        });
+};
