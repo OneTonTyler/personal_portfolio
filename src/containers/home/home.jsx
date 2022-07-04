fbe00a4294ab1a8a3a0461996ad834c58c40d103
Tyler <OneTonTyler@pm.me>
2022-07-03 20:27:34 -0600

    Initializing homepage

--- src/containers/home/home.jsx
+++ src/containers/home/home.jsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './home.css';
 
-const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    );
+class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasError: false,
+            editorActive: true,
+            data: {}
+        }
+    }
+
+    componentDidMount() {
+        this.callBackendApi()
+            .then(res => {
+                if (!res.message) {
+                    this.setState({hasError: true});  // Basic error handling
+                }
+                else {
+                    this.setState({data: res.message});
+                }
+            })
+            .catch(err => console.log(err));
+    }
+
+    // Connect to mySQL database
+    callBackendApi = async () => {
+        const response = await fetch('/api?table=HomePage&id=0');
+        const body = await response.json()
+
+        if (response.status !== 200) {
+            this.setState({hasError: true})  // Bad request
+            throw Error(body.errorMessage);
+        }
+        return body;
+    }
+
+
+    render() {
+        const {CONTENT: content} = this.state.data;
+
+        return (
+            <div>
+                <p>{content}</p>
+            </div>
+        );
+    }
 }
 
-export default Home;
+export default HomePage;
