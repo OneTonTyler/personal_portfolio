d09db4d55c1ea1796d7837dfe72bd2d9541dea4a
Tyler <OneTonTyler@pm.me>
2022-07-03 23:53:28 -0600

    Working on homepage

--- src/containers/home/home.jsx
+++ src/containers/home/home.jsx
@@ -1,6 +1,83 @@
 import React, { Component } from 'react';
 import './home.css';
 
+
+const RenderView = props => {
+    let {
+        PROFILE_IMG: profile_img,
+        TITLE: title,
+        CONTENT: content } = props.data;
+
+    if (!profile_img) {
+        profile_img = 'portfolio.jpg'
+    }
+
+    return (
+        <div className='home__render-view'>
+            <h1>Guest View</h1>
+
+            <div className='home__render-view__content'>
+                <img src={require(`../../assests/profile/${profile_img}`)} alt='profile'/>
+            </div>
+
+            <div className='home__render-view__content'>
+                <h1>{title}</h1>
+            </div>
+
+            <div className='home__render-view__content'>
+                <p>{content}</p>
+            </div>
+        </div>
+    )
+}
+
+const EditorView = props => {
+    let {
+        PROFILE_IMG: profile_img,
+        TITLE: title,
+        CONTENT: content } = props.data;
+
+    if (!profile_img) {
+        profile_img = 'portfolio.jpg'
+    }
+
+    return (
+        <div className='home__container-content'>
+            <form onSubmit={props.submit}>
+                <h1>Content Editor</h1>
+
+                {/* Profile image path */}
+                <div className='mb-3'>
+                    <label form='home_img' className='form-label'>Profile Image</label>
+                    <input type='text' id='home_img' className='form-control' defaultValue={profile_img}/>
+                </div>
+
+                {/* Title */}
+                <div className='mb-3'>
+                    <label form='home_title' className='form-label'>Title</label>
+                    <input type='text' id='home_title' className='form-control' defaultValue={title}/>
+                </div>
+
+                {/* About Me */}
+                <div className='mb-3'>
+                    <label form='home_content' className='form-label'>Content</label>
+                    <textarea className='form-control' id='home_content' defaultValue={content} />
+                </div>
+
+                {/* Upload content to sql database */}
+                <div className='btn-group' role='group'>
+                    <button type='submit' className='btn btn-primary'>Submit</button>
+                    <button type='submit' className='btn btn-outline-primary'>Render</button>
+                    <button type='submit' className='btn btn-outline-primary'>Cancel</button>
+                </div>
+
+            </form>
+            {/* Guest View */}
+            <RenderView data={props.data} />
+        </div>
+    )
+}
+
 class HomePage extends Component {
     constructor(props) {
         super(props);
@@ -9,6 +86,8 @@ class HomePage extends Component {
             editorActive: true,
             data: {}
         }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     componentDidMount() {
@@ -36,9 +115,28 @@ class HomePage extends Component {
         return body;
     }
 
+    async handleSubmit(event) {
+        console.log('Test');
+    }
+
+    async handleRender(event) {
+
+    }
+
+    async handleCancel(event) {
+
+    }
+
 
     render() {
-        const {CONTENT: content} = this.state.data;
+        let { CONTENT: content } = this.state.data;
+
+        if (this.state.editorActive) {
+            return (
+                <EditorView
+                    data={this.state.data} submit={this.handleSubmit}/>
+            )
+        }
 
         return (
             <div>
