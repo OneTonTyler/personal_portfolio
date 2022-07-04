50ab9041ff0e759f87d17999fb02b4eebbeaaf81
Tyler <OneTonTyler@pm.me>
2022-07-03 20:27:17 -0600

    Adding navigation bar to application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import { Navbar } from './components/index';
 import { Blog, Home, Projects, Resume } from './containers';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
     return (
         <div className='App'>
+            <div>
+                <Navbar />
+            </div>
+
             <Router>
                 <Routes>
                     <Route path="/" element={<Home />} />
