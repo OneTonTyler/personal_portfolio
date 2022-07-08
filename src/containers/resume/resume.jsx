379cc97e9014119be228c82473d5b6cf3caa1cff
Tyler <OneTonTyler@pm.me>
2022-07-07 18:46:04 -0600

    Working on resume page

--- src/containers/resume/resume.jsx
+++ src/containers/resume/resume.jsx
@@ -3,23 +3,43 @@ import './resume.css';
 
 const RenderView = props => {
     // Initialize variables
-    const data = props.data;
+    const experience = props.experience;
 
     // Create views for each job posting
-    const job_view = data.map(job => {
+    const job_view = experience.map(job => {
         const {
             ID: id,
             JOB_TITLE: job_title,
             JOB_DESCRIPTION: job_description,
             DUTIES: duties,
-            DATE_STARTED: started,
-            DATE_ENDED: ended
+            DATE_STARTED: date_started,
+            DATE_ENDED: date_ended
         } = job;
 
+        const options = { year: 'numeric', month: 'short' }
+        const started = new Date(date_started).toLocaleString('en-US', options);
+        const ended = new Date(date_ended).toLocaleString('en-US', options);
+
         return (
-            <div key={id}>
-                <h1>{job_title}</h1>
-                <h2>{job_description}</h2>
+            <div key={id} className='resume__experience'>
+                <div className='resume__experience-header'>
+                    <h2>City of Wheat Ridge Police Department</h2>
+                    <h2>Wheat Ridge, CO</h2>
+                </div>
+
+                <div className='resume__experience-title'>
+                    <h3>{job_title}</h3>
+                    <h3>{started} - {ended}</h3>
+                </div>
+
+                <div>
+                    <ul>
+                        <li>Some duties</li>
+                        <li>More Duties</li>
+                        <li>And more duties</li>
+                    </ul>
+                </div>
+
             </div>
         );
     })
@@ -33,20 +53,33 @@ class Resume extends Component {
         this.state = {
             hasError: false,
             editorActive: true,
-            data: {}
+            header: {},
+            skills: {},
+            workExperience: {}
         }
     }
 
-    componentDidMount() {
-        this.callBackendApi()
-            .then(res => {
-                this.setState({data: res.message});
-            })
-            .catch(err => console.log(err))
+    async componentDidMount() {
+        const responses = {};
+        for (const table of ['resume_header', 'resume_skills', 'resume_experience']) {
+            await this.callBackendApi(table)
+                .then(res => {
+                    responses[table] = res.message
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+
+        this.setState({
+            header: responses['resume_header'],
+            skills: responses['resume_skills'],
+            workExperience: responses['resume_experience']
+        });
     }
 
-    callBackendApi = async () => {
-        const response = await fetch('/api/list_all?table=resume');
+    callBackendApi = async (table) => {
+        const response = await fetch(`/api/list_all?table=${table}`);
         const body = await response.json()
 
         if (response.status !== 200) {
@@ -57,7 +90,7 @@ class Resume extends Component {
     }
 
     render() {
-        if (!this.state.data[0]) {
+        if (!this.state.header[0]) {
             return (
                 <div>
                     <p>Hello World</p>
@@ -66,7 +99,13 @@ class Resume extends Component {
         }
         else {
             return (
-                <RenderView data={this.state.data}/>
+                <div className='resume__container'>
+                    <RenderView
+                        header={this.state.header}
+                        skills={this.state.skills}
+                        experience={this.state.workExperience}
+                    />
+                </div>
             )
         }
     }
